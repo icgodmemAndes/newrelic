06cbd1cec07806b163190dcb337018fd2fc7c9d2
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2024-11-10 22:17:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Flask Web Application Setup
+
+1. Install `virtualenv`, if not already done
+1. Create a virtual environment for the application, if needed
+1. Activate virtual environment
+1. Upgrade `pip` in virtual environment
+1. Install dependencies listed in `requirements.txt`
+
+
+| Step | Windows (PowerShell)                  | Linux / MacOS (bash)                   |
+|------|---------------------------------------|----------------------------------------|
+| 1    | `pip install virtualenv`              | `pip3 install virtualenv`              |
+| 2    | `python -m venv venv`                 | `python3 -m venv venv`                 |
+| 3    | `.\venv\Scripts\Activate.ps1`         | `source venv/bin/activate`             |
+| 4    | `python -m pip install --upgrade pip` | `python3 -m pip install --upgrade pip` |
+| 5    | `pip install -r requirements.txt`     | `pip install -r requirements.txt`      |
+
+
+# Running the Application
+To send data to New Relic APM, uncomment the the following lines in `webserver.py`:
+```python
+import newrelic.agent
+newrelic.agent.initialize()
+```
+
+| Windows (PowerShell)                           | Linux / macOS (bash)                           |
+|------------------------------------------------|------------------------------------------------|
+| `$Env:NEW_RELIC_APP_NAME = "Local Python App"` | `export NEW_RELIC_APP_NAME="Local Python App"` |
+| `$Env:NEW_RELIC_LICENSE_KEY = "XXXX...NRAL"`   | `export NEW_RELIC_LICENSE_KEY="XXXX...NRAL"`   |
+| `python webserver.py`                          | `python3 webserver.py`                         |
+
+
+# Endpoints to Test
+
+| API                             | Endpoint                                 | Notes                 |
+|---------------------------------|------------------------------------------|-----------------------|
+| Ping                            | http://127.0.0.1:5000/ping               | JSON: pong            |
+| Convert Celcius to Fahrenheit   | http://127.0.0.1:5000/convertC/123       |                       |
+| Convert Fahrenheit to Celcius   | http://127.0.0.1:5000/convertF/--143     | Generate Server Error |
+| Calculate n-th Prime Number     | http://127.0.0.1:5000/api/prime/12345    | Put load on CPU / RAM |
+| Calculate n-th Fibonacci number | http://127.0.0.1:5000/api/fib/42         | JSON result           |
+| Validate with Luhn algorithm    | http://127.0.0.1:5000/api/luhn/951847623 | Masked in logs        |
+| Get COVID data for Ontario      | http://127.0.0.1:5000/covid              |                       |
+| Get status for services         | http://127.0.0.1:5000/statuspage         | AJAX request          |
+
+# OpenTelemetry
+1. To send telemetry data to New Relic, set the following environment variables
+
+Windows (PowerShell)
+```
+$Env:OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp.nr-data.net:4317"
+$Env:OTEL_EXPORTER_OTLP_HEADERS=api-key="XXXX...NRAL"
+(optional) $Env:OTEL_RESOURCE_ATTRIBUTES=service.name="python-flask.otel,service.instance.id=localhost-pc"
+```
+
+Linux / macOS
+```
+export OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp.nr-data.net:4317"
+export OTEL_EXPORTER_OTLP_HEADERS=api-key="XXXX...NRAL"
+(optional) export OTEL_RESOURCE_ATTRIBUTES=service.name="python-flask.otel,service.instance.id=localhost-pc"
+```
+
+2. Download the following packages to your virtual environment
+```
+pip install opentelemetry-api
+pip install opentelemetry-sdk
+pip install opentelemetry-instrumentation-flask
+pip install opentelemetry-exporter-otlp-proto-grpc
+pip install opentelemetry-distro
+```
+
+3. No changes to the code is needed, just run the app as usual but with `opentelemetry-instrument` at the front
+```
+opentelemetry-instrument python3 webserver.py
+```
+
+# Docker Image
+1. Build the Docker image with `docker build -t python-flask:latest .`
+2. Run the app with your `INGEST - LICENSE` key and give your application a name:
+```
+docker run -d -e NEW_RELIC_LICENSE_KEY="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXNRAL" -e NEW_RELIC_APP_NAME="python-flask.docker" -p 5000:5000 python-flask:latest`
+```
+3. Access the application with the same endpoints above at `http://127.0.0.1:5000`
---
+++ webserver.py
@@ -0,0 +1,144 @@
+import newrelic.agent
+newrelic.agent.initialize()
+
+# OpenTelemetry
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.jinja2 import Jinja2Instrumentor
+from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
+from opentelemetry.instrumentation.redis import RedisInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.sdk._logs import LogEmitterProvider
+from opentelemetry.sdk._logs import set_log_emitter_provider
+from opentelemetry.sdk._logs import LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogProcessor
+
+# Import the logging module and the New Relic log formatter
+import logging
+# from newrelic.agent import NewRelicContextFormatter
+
+# Instantiate a new log handler, and set logging level
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.INFO)
+
+# Instantiate the log formatter and add it to the log handler
+#formatter = NewRelicContextFormatter()
+#handler.setFormatter(formatter)
+
+# Get the root logger, set logging level, and add the handler to it
+# root_logger = logging.getLogger()
+# root_logger.setLevel(logging.INFO)
+# root_logger.addHandler(handler)
+
+# Flask Web Application
+from flask import Flask, render_template, jsonify
+flaskapp = Flask(__name__, static_url_path='/', static_folder='application/static', template_folder='application/templates')
+
+# OpenTelemetry Settings
+import uuid
+serviceId = str(uuid.uuid1())
+
+trace.set_tracer_provider(TracerProvider(resource=Resource.create({"service.name": "python-flask.otel", "service.instance.id": serviceId, "environment": "local"})))
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))
+
+log_emitter_provider = LogEmitterProvider(resource=Resource.create({"service.name": "python-flask.otel", "service.instance.id": serviceId, "environment": "local"}))
+set_log_emitter_provider(log_emitter_provider)
+
+exporter = OTLPLogExporter(insecure=True)
+log_emitter_provider.add_log_processor(BatchLogProcessor(exporter))
+log_emitter = log_emitter_provider.get_log_emitter(__name__, "0.1")
+handler = LoggingHandler(level=logging.NOTSET, log_emitter=log_emitter)
+
+# Attach OTLP handler to root logger
+logging.getLogger().addHandler(handler)
+
+# Log directly
+logging.info("Jackdaws love my big sphinx of quartz.")
+
+# Create different namespaced loggers
+logger1 = logging.getLogger("myapp.area1")
+logger2 = logging.getLogger("myapp.area2")
+
+logger1.debug("Quick zephyrs blow, vexing daft Jim.")
+logger1.info("How quickly daft jumping zebras vex.")
+logger2.warning("Jail zesty vixen who grabbed pay from quack.")
+logger2.error("The five boxing wizards jump quickly.")
+
+FlaskInstrumentor().instrument_app(flaskapp)
+RequestsInstrumentor().instrument()
+Jinja2Instrumentor().instrument()
+URLLib3Instrumentor().instrument()
+RedisInstrumentor().instrument()
+LoggingInstrumentor().instrument()
+
+# Navigation
+@flaskapp.route("/")
+def index():
+    return render_template("index.html", title="Flask Web Application")
+
+@flaskapp.route("/ping", strict_slashes=False)
+def ping():
+    return jsonify(ping="pong")
+
+@flaskapp.route("/about")
+def about():
+    return render_template("about.html", title="Datacrunch - About")
+
+@flaskapp.route("/statuspage", strict_slashes=False)
+def statuspage():
+    return render_template("projects/statuspage.html", title="Simple Statuspage")
+
+# API to convert Fahrenheit to Celcius
+@flaskapp.route("/convertC/<tempF>")
+def convertC(tempF):
+    tempC = (5/9*(float(tempF))-32)
+    logging.info(f"[INFO] Converted {tempF}°F to {tempC:.2f}°C.")
+    return f"{tempF}°F is {tempC:.2f}°C."
+
+# API to convert Celcius to Fahrenheit New Comment
+@flaskapp.route("/convertF/<tempC>")
+def convertF(tempC):
+    try:
+        tempF = 9/5*(float(tempC))+32
+        logging.info(f"[INFO] Converted {tempC}°F to {tempF:.2f}°C.")
+        return f"{tempC}°C is {tempF:.2f}°F."
+    except:
+        logging.warning("[WARN] Invalid temperature!")
+
+### Add Applications Here #######
+
+# API to calculate the nth prime number and how long it takes
+from application.projects.prime import prime
+flaskapp.register_blueprint(prime)
+
+# API to calculate the nth fibonacci number
+from application.projects.fibonacci import fibonacci
+flaskapp.register_blueprint(fibonacci)
+
+# API to validate credit card numbers
+from application.projects.luhn import luhn
+flaskapp.register_blueprint(luhn)
+
+# Get COVID data and plot on chart
+from application.projects.covid import covid
+flaskapp.register_blueprint(covid)
+
+# Test redis-py in App
+from application.projects.redispy import redispy
+flaskapp.register_blueprint(redispy)
+
+# Input number to check divisibility
+from application.projects.divisibility import divisibility
+flaskapp.register_blueprint(divisibility)
+
+# Run Flask Web Application, new comment
+if __name__ == "__main__":
+    flaskapp.run()
+     
